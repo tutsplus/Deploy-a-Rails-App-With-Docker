bfba58df3014e7faef4c26e5465788cc71cf3e8f
José Mota <jose@josemota.net>
2015-01-22 16:30:24 +0000

    Lesson 8.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,1 +1,23 @@
-echo "Docker was built completely."
+git clone --depth 1 http://bitbucket.org/josemota/dockerapp app
+
+cd app
+
+source "/usr/local/share/chruby/chruby.sh"
+chruby ruby
+
+gem install bundler
+
+bundle install --without=development,test
+bundle exec rake db:migrate
+
+if [[ $? != 0 ]]; then
+  echo
+  echo "== Failed to migrate. Running setup first."
+  echo
+  bundle exec rake db:setup && \
+  bundle exec rake db:migrate
+fi
+
+export SECRET_KEY_BASE=$(rake secret)
+
+bundle exec rails server -b 0.0.0.0
--- script.sh
+++ script.sh
@@ -4,6 +4,10 @@ db() {
   docker run -P --volumes-from app_data --name app_db -e POSTGRES_USER=app_user -e POSTGRES_PASSWORD=$APP_PWD -d -t postgres:latest
 }
 
+app() {
+  docker run -p 3000:3000 --link app_db:postgres --name app josemotanet/app
+}
+
 action=$1
 
 ${action}
